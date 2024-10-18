c7addd4511232184fd946fa83af8967dd55ce45a
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-19 02:43:21 +0300

    fix: fix workers

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 import cluster from 'cluster';
-import os from 'os';
-import { app } from './app.ts';
+import { availableParallelism } from 'os';
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
+import { parse } from 'node:url';
+import { userRoutes } from './routes/user.routes.ts';
 
+const PORT = Number(process.env.PORT) || 4000;
+const cpus = availableParallelism();
+const isMulti = process.argv.includes('--multi');
 
-const PORT = Number(process.env.PORT) || 3000;
-const isCluster = process.argv.includes('--mult');
-
-if (isCluster && cluster.isPrimary) {
-  const numCPUs = os.cpus().length;
-
+if (isMulti && cluster.isPrimary) {
   console.log(`Master ${process.pid} is running`);
 
-  for (let i = 0; i < numCPUs; i++) {
-    const workerPort = PORT + i;
-    cluster.fork({ PORT: workerPort });
+  for (let i = 1; i < cpus; i++) {
+    cluster.fork({ WORKER_PORT: PORT + i });
   }
 
-  cluster.on('exit', (worker, code, signal) => {
+  cluster.on('exit', (worker, _code, _signal) => {
     console.log(`Worker ${worker.process.pid} died`);
   });
+
+  createServer((req: IncomingMessage, res: ServerResponse) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Load balancer is listening');
+  }).listen(PORT, () => {
+    console.log(`Load balancer is listening on port ${PORT}`);
+  });
 } else {
-  // Worker process
-  const workerPort = Number(process.env.PORT) || PORT;
-
-
+  const workerPort = Number(process.env.WORKER_PORT) || PORT + (cluster.worker?.id ?? 0);
+  const app = createServer((req: IncomingMessage, res: ServerResponse) => {
+    const parsedUrl = parse(req.url || '', true);
+    userRoutes(req, res, parsedUrl);
+  });
 
   app.listen(workerPort, () => {
     console.log(`Worker ${process.pid} is listening on port ${workerPort}`);
-  });
-
-  app.on('request', (req, res) => {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello World from worker ' + process.pid);
-    }
   });
 }
